c82f2738669c6acf35f5633fbc1ce531e57d83de
Jan Hochbrueckner <jhochbrueckner@gmail.com>
2013-02-09 12:46:21 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sportsfriender::Application.config.secret_token = 'a3c7a597e5a04844fb425e64107ecb95340aaa5e43c8976e294fed485324803f3610285b56db61a2e0bc53708b85278ecb72e9cd455cb9fad8ef53aff92f3ca4'
